54ef3ff00cfd0c88de4ff251f092cfdabf6dd759
Irina Kainer <irina.kainer@redjar.com.ar>
2022-09-13 18:52:55 -0300

    Register structure

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -4,7 +4,7 @@ userController.save = (req, res) => {
     const data = Object.values(req.body);
     console.log('body req', req.body)
     req.getConnection((err, connection) => {
-        const query = connection.query(`INSERT INTO usuarios (idTipoUsuario, NombreUsuario, ApellidoUsuario, MailUsuario, PassUsuario) VALUES (?, ?, ?, ?, ?)`, data,
+        const query = connection.query(`INSERT INTO usuarios (idTipoUsuario, NombreUsuario, ApellidoUsuario, MailUsuario, PassUsuario, TelefonoUsuario, ProvinciaUsuario, LocalidadUsuario, CPUsuario, DomCalleUsuario, DomNumUsuario, DomPisoDptoUsuario) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, data,
             (err, customer) => {
                 console.log(err)
                 console.log(customer)
